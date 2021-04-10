595a7cc1c6aee2a71651be106d3383c55f710b52
Ravi Kiran <rchandra@ftdi.com>
2021-04-10 19:15:32 +0530

    basic first deploy

---
+++ examples/2-basic-api/app.js
@@ -0,0 +1,23 @@
+const result = document.querySelector('.result');
+
+const fetchData = async() => {
+    try{
+        const {data} = await axios('/api/2-basic-api');
+        const products = data.map( product => {
+            const { image: { url }, name, price } = product;
+            return `<article class="product">
+                        <img src="${url}" alt="${name}" />
+                        <div class="info">
+                            <h5>${name}</h5>
+                            <h5 class="price">${price}</h5>
+                        </div>
+                    </article>
+                    `
+        }).join('')
+        result.innerHTML = products;
+    } catch(err){
+        console.log(err.response.data)
+    }
+}
+
+fetchData();
